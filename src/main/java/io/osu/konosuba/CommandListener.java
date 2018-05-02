31315011bf4f1764529b523787a50ffa9a875b28
Alex <Alex@Alex-PC>
2018-05-02 18:32:13 -0400

    test

--- src/main/java/io/osu/konosuba/CommandListener.java
+++ src/main/java/io/osu/konosuba/CommandListener.java
@@ -5,13 +5,15 @@ import java.io.IOException;
 
 import io.magiccraftmaster.util.Calculator;
 import io.osu.konosuba.commands.Example;
+import io.osu.konosuba.commands.Stats;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class CommandListener extends ListenerAdapter {
 	
 	private final Command[] commands = {
-		new Example()	
+		new Example(),
+		new Stats()
 	};
 
 	CommandListener() throws IOException {
--- src/main/java/io/osu/konosuba/commands/Stats.java
+++ src/main/java/io/osu/konosuba/commands/Stats.java
@@ -1,5 +1,23 @@
 package io.osu.konosuba.commands;
 
-public class Stats {
+import java.awt.Color;
+
+import io.osu.konosuba.Command;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class Stats extends Command {
+
+	//Replace each formal parameter
+	protected Example() {
+		super(embedColor, prefix, name, description, requiredPermission, requiredID);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void run(MessageReceivedEvent event, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
