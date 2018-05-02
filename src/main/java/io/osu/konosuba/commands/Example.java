8fd442aff2502caf58f43e2b3aec5d98012fbd23
Vonch <vonchude@gmail.com>
2018-05-02 18:21:22 -0400

    Check implementation

--- src/main/java/io/osu/konosuba/commands/Example.java
+++ src/main/java/io/osu/konosuba/commands/Example.java
@@ -1,7 +1,23 @@
 package io.osu.konosuba.commands;
 
-import io.osu.konosuba.Command;
+import java.awt.Color;
 
-public class Example extends Command{
+import io.osu.konosuba.Command;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class Example extends Command {
+
+	//Replace each formal parameter
+	protected Example() {
+		super(embedColor, prefix, name, description, requiredPermission, requiredID);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void run(MessageReceivedEvent event, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
