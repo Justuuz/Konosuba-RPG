84d70483adbc5e9977742feb6020ce6b79a0a7fa
Vonch <vonchude@gmail.com>
2018-05-02 01:03:41 -0400

    Added code

--- src/main/java/io/osu/konosuba/KonoSuba.java
+++ src/main/java/io/osu/konosuba/KonoSuba.java
@@ -1,7 +1,30 @@
 package io.osu.konosuba;
 
-public class KonoSuba {
-	public static void main(String[] args) {
-		//
+import java.io.IOException;
+
+import javax.security.auth.login.LoginException;
+
+
+
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import net.dv8tion.jda.bot.sharding.DefaultShardManagerBuilder;
+
+public class KonoSuba  {
+	
+	static SimpleReader key = new SimpleReader1L("Desktop/Kobosuba/key.txt");
+	private static final String KOBOSUBA_TOKEN = key.nextLine();
+	
+	
+	public static void main(String[] args) throws LoginException, IOException {
+		/*ShardManager SHARD_MANAGER = */new DefaultShardManagerBuilder()
+				.setToken()
+				.setShardsTotal(1)
+				.addEventListeners(new Listener(KOBOSUBA_TOKEN))
+				
+				.setBulkDeleteSplittingEnabled(false)
+				
+				.build();
+		
 	}
 }
