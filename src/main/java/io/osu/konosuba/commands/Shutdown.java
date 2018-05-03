86c40979f5fb4aba6a167fb4c7d7824ef72fa580
Vonch <vonchude@gmail.com>
2018-05-02 23:29:20 -0400

    Proper turnoff for bot

---
+++ src/main/java/io/osu/konosuba/commands/Shutdown.java
@@ -0,0 +1,30 @@
+package io.osu.konosuba.commands;
+
+import io.osu.konosuba.commands.*;
+import io.osu.konosuba.*;
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class Shutdown extends Command{
+
+	public Shutdown() {
+		super(null, null, "shutdown", "Shutdowns the bot", null, 81911518148440064L);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run(MessageReceivedEvent event, String[] args) {
+		// TODO Auto-generated method stub
+		if(args.length > 1) {
+			send(event.getGuild(), event.getChannel(), "**ERROR**: too many argument(s)", true);
+			return;
+		}
+		if(args[0].equalsIgnoreCase("shutdown")) {
+			send(event.getGuild(), event.getChannel(), "**Shutting Down!**", true);
+			Konosuba.CLIENT_DATA_MANAGER.save();
+			event.getJDA().shutdown();
+		}
+		
+	}
+
+}
