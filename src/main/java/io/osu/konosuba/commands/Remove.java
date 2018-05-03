82c770d21d279e65d19ec050871c0125400460e0
Alex <Alexchan99xd@yahoo.com>
2018-05-03 15:09:27 -0400

    quick fix
    Change-Id: Ia18f913fdce44e2293109f4bc933a8338eb8be77

--- src/main/java/io/osu/konosuba/commands/Remove.java
+++ src/main/java/io/osu/konosuba/commands/Remove.java
@@ -31,123 +31,123 @@ public class Remove extends Command {
 		 * helmet, chest, leggings, boots, onHand, offHand, cape, necklace, ring;
 		 */
 		if(Konosuba.CLIENT_DATA_MANAGER.hasData(event.getChannel().getIdLong())) {
-		ClientData player = new ClientData(event.getChannel().getIdLong());
-		EmbedBuilder help  = new EmbedBuilder();
-		
-		if(args[1].equalsIgnoreCase("helmet")) {
+			ClientData player = new ClientData(event.getChannel().getIdLong());
+			EmbedBuilder help  = new EmbedBuilder();
 			
-			if(!player.getHelmet().equalsIgnoreCase("none")) {
-				String message = "No item to remove";
-
-				send(event.getGuild(), event.getChannel(), message, true);
+			if(args[1].equalsIgnoreCase("helmet")) {
+				
+				if(!player.getHelmet().equalsIgnoreCase("none")) {
+					String message = "No item to remove";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+					
+				}
+				else {
+					player.setHelmet("None");
+				}
 				
 			}
-			else {
-				player.setHelmet("None");
-			}
 			
-		}
-		
-		if(args[1].equalsIgnoreCase("chest")) {
-			
-			if(!player.getChest().equalsIgnoreCase("none")) {
-				String message = "No item to remove";
-
-				send(event.getGuild(), event.getChannel(), message, true);
+			if(args[1].equalsIgnoreCase("chest")) {
+				
+				if(!player.getChest().equalsIgnoreCase("none")) {
+					String message = "No item to remove";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+					
+				}
+				else {
+					player.setChest("None");
+				}
 				
 			}
-			else {
-				player.setChest("None");
-			}
 			
-		}
-		
-		if(args[1].equalsIgnoreCase("leggings")) {
-			
-			if(!player.getLeggings().equalsIgnoreCase("none")) {
-				String message = "No item to remove";
-
-				send(event.getGuild(), event.getChannel(), message, true);
+			if(args[1].equalsIgnoreCase("leggings")) {
+				
+				if(!player.getLeggings().equalsIgnoreCase("none")) {
+					String message = "No item to remove";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+					
+				}
+				else {
+					player.setLeggings("None");
+				}
 				
 			}
-			else {
-				player.setLeggings("None");
-			}
 			
-		}
-		
-		if(args[1].equalsIgnoreCase("onhand")) {
-			
-			if(!player.getOnHand().equalsIgnoreCase("none")) {
-				String message = "No item to remove";
-
-				send(event.getGuild(), event.getChannel(), message, true);
+			if(args[1].equalsIgnoreCase("onhand")) {
+				
+				if(!player.getOnHand().equalsIgnoreCase("none")) {
+					String message = "No item to remove";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+					
+				}
+				else {
+					player.setOnHand("None");
+				}
 				
 			}
-			else {
-				player.setOnHand("None");
-			}
 			
-		}
-		
-		if(args[1].equalsIgnoreCase("offhand")) {
-			
-			if(!player.getOffHand().equalsIgnoreCase("none")) {
-				String message = "No item to remove";
-
-				send(event.getGuild(), event.getChannel(), message, true);
+			if(args[1].equalsIgnoreCase("offhand")) {
+				
+				if(!player.getOffHand().equalsIgnoreCase("none")) {
+					String message = "No item to remove";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+					
+				}
+				else {
+					player.setOffHand("None");
+				}
 				
 			}
-			else {
-				player.setOffHand("None");
-			}
 			
-		}
-		
-		if(args[1].equalsIgnoreCase("cape")) {
-			
-			if(!player.getCape().equalsIgnoreCase("none")) {
-				String message = "No item to remove";
-
-				send(event.getGuild(), event.getChannel(), message, true);
+			if(args[1].equalsIgnoreCase("cape")) {
+				
+				if(!player.getCape().equalsIgnoreCase("none")) {
+					String message = "No item to remove";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+					
+				}
+				else {
+					player.setCape("None");
+				}
 				
 			}
-			else {
-				player.setCape("None");
-			}
 			
-		}
-		
-		if(args[1].equalsIgnoreCase("necklace")) {
-			
-			if(!player.getNecklace().equalsIgnoreCase("none")) {
-				String message = "No item to remove";
-
-				send(event.getGuild(), event.getChannel(), message, true);
+			if(args[1].equalsIgnoreCase("necklace")) {
+				
+				if(!player.getNecklace().equalsIgnoreCase("none")) {
+					String message = "No item to remove";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+					
+				}
+				else {
+					player.setNecklace("None");
+				}
 				
 			}
-			else {
-				player.setNecklace("None");
-			}
 			
-		}
-		
-		if(args[1].equalsIgnoreCase("ring")) {
-			
-			if(!player.getRing().equalsIgnoreCase("none")) {
-				String message = "No item to remove";
-
-				send(event.getGuild(), event.getChannel(), message, true);
+			if(args[1].equalsIgnoreCase("ring")) {
+				
+				if(!player.getRing().equalsIgnoreCase("none")) {
+					String message = "No item to remove";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+					
+				}
+				else {
+					player.setRing("None");
+				}
 				
 			}
-			else {
-				player.setRing("None");
-			}
+			//update JSON
+			Konosuba.CLIENT_DATA_MANAGER.trySave();
 			
 		}
-		//update JSON
-		Konosuba.CLIENT_DATA_MANAGER.trySave();
-		
 	}
-}
 }
