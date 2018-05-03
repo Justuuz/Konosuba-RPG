08f793c7130c06d05ad92f2f48429282b304aed3
Alex <Alex@Alex-PC>
2018-05-03 13:15:27 -0400

    equip and remove command

--- src/main/java/io/osu/konosuba/commands/Equip.java
+++ src/main/java/io/osu/konosuba/commands/Equip.java
@@ -1,5 +1,63 @@
 package io.osu.konosuba.commands;
 
-public class Equip {
+import io.osu.konosuba.Command;
+import io.osu.konosuba.Konosuba;
+import io.osu.konosuba.data.ClientData;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
+public class Equip extends Command {
+	
+	public Equip() {
+		super(Konosuba.COLOR, Konosuba.PREFIX, "battle", "all related battle commands", null, 0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/*
+	 * framework
+	 */
+	
+	
+	@Override
+	public void run(MessageReceivedEvent event, String[] args) {
+		
+		/*
+		 * equips an item
+		 * 
+		 * (important) cannot equip an item if there is already an item in that place
+		 * helmet, chest, leggings, boots, onHand, offHand, cape, necklace, ring;
+		 */
+		
+		if(args[1].equalsIgnoreCase("helmet")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("chest")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("leggings")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("onhand")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("offhand")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("cape")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("necklace")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("ring")) {
+			
+		}
+		
+	}
 }
--- src/main/java/io/osu/konosuba/commands/Remove.java
+++ src/main/java/io/osu/konosuba/commands/Remove.java
@@ -1,5 +1,63 @@
 package io.osu.konosuba.commands;
 
-public class Remove {
+import io.osu.konosuba.Command;
+import io.osu.konosuba.Konosuba;
+import io.osu.konosuba.data.ClientData;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
+public class Remove extends Command {
+	
+	public Remove() {
+		super(Konosuba.COLOR, Konosuba.PREFIX, "battle", "all related battle commands", null, 0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/*
+	 * framework
+	 */
+	
+	
+	@Override
+	public void run(MessageReceivedEvent event, String[] args) {
+		
+		/*
+		 * removes an item
+		 * 
+		 * can't remove item if there isn't one
+		 * helmet, chest, leggings, boots, onHand, offHand, cape, necklace, ring;
+		 */
+		
+		if(args[1].equalsIgnoreCase("helmet")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("chest")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("leggings")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("onhand")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("offhand")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("cape")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("necklace")) {
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("ring")) {
+			
+		}
+		
+	}
 }
