1a0c2b5da986e20c8dea586f85084ceb661311ce
Alex <Alexchan99xd@yahoo.com>
2018-05-03 15:15:27 -0400

    finsihed? equip

--- src/main/java/io/osu/konosuba/commands/Equip.java
+++ src/main/java/io/osu/konosuba/commands/Equip.java
@@ -32,156 +32,157 @@ public class Equip extends Command {
 		 * (important) cannot equip an item if there is already an item in that place
 		 * helmet, chest, leggings, boots, onHand, offHand, cape, necklace, ring;
 		 */
-		
+		if(Konosuba.CLIENT_DATA_MANAGER.hasData(event.getChannel().getIdLong())) {
 		ClientData player = new ClientData(event.getChannel().getIdLong());
 		
 		if(args[1].equalsIgnoreCase("helmet")) {
+				
+				if(!player.getHelmet().equalsIgnoreCase("none")) {
+					String message = "Please remove the current item first";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+				}
+				else {
+					
+					String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
+					
+					player.setHelmet(item);
+					String message = "Equipped" + item;
+					send(event.getGuild(), event.getChannel(), message , true);
+				}
+				
+			}
 			
-			if(!player.getHelmet().equalsIgnoreCase("none")) {
-				String message = "Please remove the current item first";
-
-				send(event.getGuild(), event.getChannel(), message, true);
-			}
-			else {
+			if(args[1].equalsIgnoreCase("chest")) {
 				
-				String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
+				if(!player.getChest().equalsIgnoreCase("none")) {
+					String message = "Please remove the current item first";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+				}
+				else {
+					
+					String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
+					
+					player.setChest(item);
+					String message = "Equipped" + item;
+					send(event.getGuild(), event.getChannel(), message , true);
+				}
 				
-				player.setHelmet(item);
-				String message = "Equipped" + item;
-				send(event.getGuild(), event.getChannel(), message , true);
 			}
+			
+			if(args[1].equalsIgnoreCase("leggings")) {
+				
+				if(!player.getLeggings().equalsIgnoreCase("none")) {
+					String message = "Please remove the current item first";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+				}
+				else {
+					
+					String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
+					
+					player.setLeggings(item);
+					String message = "Equipped" + item;
+					send(event.getGuild(), event.getChannel(), message , true);
+				}
+				
+			}
+			
+			if(args[1].equalsIgnoreCase("onhand")) {
+				
+				if(!player.getOnHand().equalsIgnoreCase("none")) {
+					String message = "Please remove the current item first";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+				}
+				else {
+					
+					String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
+					
+					player.setOnHand(item);
+					String message = "Equipped" + item;
+					send(event.getGuild(), event.getChannel(), message , true);
+				}
+				
+			}
+			
+			if(args[1].equalsIgnoreCase("offhand")) {
+				
+				
+				if(!player.getOffHand().equalsIgnoreCase("none")) {
+					String message = "Please remove the current item first";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+				}
+				else {
+					
+					String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
+					
+					player.setOffHand(item);
+					String message = "Equipped" + item;
+					send(event.getGuild(), event.getChannel(), message , true);
+				}
+				
+			}
+			
+			if(args[1].equalsIgnoreCase("cape")) {
+				
+				if(!player.getCape().equalsIgnoreCase("none")) {
+					String message = "Please remove the current item first";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+				}
+				else {
+					
+					String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
+					
+					player.setCape(item);
+					String message = "Equipped" + item;
+					send(event.getGuild(), event.getChannel(), message , true);
+				}
+				
+			}
+			
+			if(args[1].equalsIgnoreCase("necklace")) {
+				
+				if(!player.getNecklace().equalsIgnoreCase("none")) {
+					String message = "Please remove the current item first";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+				}
+				else {
+					
+					String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
+					
+					player.setNecklace(item);
+					String message = "Equipped" + item;
+					send(event.getGuild(), event.getChannel(), message , true);
+				}
+				
+			}
+			
+			if(args[1].equalsIgnoreCase("ring")) {
+				
+				if(!player.getRing().equalsIgnoreCase("none")) {
+					String message = "Please remove the current item first";
+	
+					send(event.getGuild(), event.getChannel(), message, true);
+				}
+				else {
+					
+					String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
+					
+					player.setRing(item);
+					String message = "Equipped" + item;
+					send(event.getGuild(), event.getChannel(), message , true);
+				}
+				
+			}
+			
+			//update client data
+			Konosuba.CLIENT_DATA_MANAGER.trySave();
 			
 		}
-		
-		if(args[1].equalsIgnoreCase("chest")) {
-			
-			if(!player.getChest().equalsIgnoreCase("none")) {
-				String message = "Please remove the current item first";
-
-				send(event.getGuild(), event.getChannel(), message, true);
-			}
-			else {
-				
-				String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
-				
-				player.setChest(item);
-				String message = "Equipped" + item;
-				send(event.getGuild(), event.getChannel(), message , true);
-			}
-			
-		}
-		
-		if(args[1].equalsIgnoreCase("leggings")) {
-			
-			if(!player.getLeggings().equalsIgnoreCase("none")) {
-				String message = "Please remove the current item first";
-
-				send(event.getGuild(), event.getChannel(), message, true);
-			}
-			else {
-				
-				String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
-				
-				player.setLeggings(item);
-				String message = "Equipped" + item;
-				send(event.getGuild(), event.getChannel(), message , true);
-			}
-			
-		}
-		
-		if(args[1].equalsIgnoreCase("onhand")) {
-			
-			if(!player.getOnHand().equalsIgnoreCase("none")) {
-				String message = "Please remove the current item first";
-
-				send(event.getGuild(), event.getChannel(), message, true);
-			}
-			else {
-				
-				String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
-				
-				player.setOnHand(item);
-				String message = "Equipped" + item;
-				send(event.getGuild(), event.getChannel(), message , true);
-			}
-			
-		}
-		
-		if(args[1].equalsIgnoreCase("offhand")) {
-			
-			
-			if(!player.getOffHand().equalsIgnoreCase("none")) {
-				String message = "Please remove the current item first";
-
-				send(event.getGuild(), event.getChannel(), message, true);
-			}
-			else {
-				
-				String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
-				
-				player.setOffHand(item);
-				String message = "Equipped" + item;
-				send(event.getGuild(), event.getChannel(), message , true);
-			}
-			
-		}
-		
-		if(args[1].equalsIgnoreCase("cape")) {
-			
-			if(!player.getCape().equalsIgnoreCase("none")) {
-				String message = "Please remove the current item first";
-
-				send(event.getGuild(), event.getChannel(), message, true);
-			}
-			else {
-				
-				String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
-				
-				player.setCape(item);
-				String message = "Equipped" + item;
-				send(event.getGuild(), event.getChannel(), message , true);
-			}
-			
-		}
-		
-		if(args[1].equalsIgnoreCase("necklace")) {
-			
-			if(!player.getNecklace().equalsIgnoreCase("none")) {
-				String message = "Please remove the current item first";
-
-				send(event.getGuild(), event.getChannel(), message, true);
-			}
-			else {
-				
-				String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
-				
-				player.setNecklace(item);
-				String message = "Equipped" + item;
-				send(event.getGuild(), event.getChannel(), message , true);
-			}
-			
-		}
-		
-		if(args[1].equalsIgnoreCase("ring")) {
-			
-			if(!player.getRing().equalsIgnoreCase("none")) {
-				String message = "Please remove the current item first";
-
-				send(event.getGuild(), event.getChannel(), message, true);
-			}
-			else {
-				
-				String item = StringUtils.toString(StringUtils.clip(args, StringUtils.ClipType.LEFT, 2), " ");
-				
-				player.setRing(item);
-				String message = "Equipped" + item;
-				send(event.getGuild(), event.getChannel(), message , true);
-			}
-			
-		}
-		
-		//update client data
-		Konosuba.CLIENT_DATA_MANAGER.trySave();
-		
 	}
 }
