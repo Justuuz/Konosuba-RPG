7a2e4657cb7e93741360306bb54538c9c86d1bdf
Alexc99xd <Alexchan99xd@yahoo.com>
2018-05-04 18:02:33 -0400

    admin command

---
+++ src/main/java/io/osu/konosuba/commands/Buy_admin.java
@@ -0,0 +1,53 @@
+package io.osu.konosuba.commands;
+
+import io.osu.konosuba.Command;
+import io.osu.konosuba.Konosuba;
+import io.osu.konosuba.data.ClientData;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class Buy_admin extends Command {
+	
+	public Buy_admin() {
+		super(Konosuba.COLOR, Konosuba.PREFIX, "battle", "all related battle commands", null, 0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/*
+	 * framework
+	 */
+	
+	
+	@Override
+	public void run(MessageReceivedEvent event, String[] args) {
+		
+		ClientData player = Konosuba.CLIENT_DATA_MANAGER.getData(event.getChannel().getIdLong());
+	/*
+	 * Show UI here
+	 */
+		
+		if(args[1].equalsIgnoreCase("admin_hat")) {
+			player.getInventory().get(0).add("Admin_hat");
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("admin_plate")) {
+			player.getInventory().get(1).add("Admin_plate");
+		}
+		
+		if(args[1].equalsIgnoreCase("admin_sword")) {
+			player.getInventory().get(3).add("Admin_sword");
+		}
+		
+		if(args[1].equalsIgnoreCase("admin_hat2")) {
+			player.getInventory().get(0).add("Admin_hat2");
+			
+		}
+		
+		if(args[1].equalsIgnoreCase("admin_hat3")) {
+			player.getInventory().get(0).add("Admin_hat3");
+			
+		}
+		Konosuba.CLIENT_DATA_MANAGER.trySave();
+		
+	}
+}
