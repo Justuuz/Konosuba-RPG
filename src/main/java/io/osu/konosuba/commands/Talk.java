a6f1ad176f34ef6bd27df83418b6ce65af21afce
Alexc99xd <Alexchan99xd@yahoo.com>
2018-05-09 15:02:03 -0400

    next commands

---
+++ src/main/java/io/osu/konosuba/commands/Talk.java
@@ -0,0 +1,38 @@
+package main.java.io.osu.konosuba.commands;
+
+import io.osu.konosuba.Command;
+import io.osu.konosuba.Konosuba;
+import io.osu.konosuba.data.ClientData;
+import io.osu.konosuba.util.PointsHandler;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class Talk extends Command {
+
+    public Talk() {
+        super(Konosuba.COLOR, Konosuba.PREFIX, "talk", "all (NPC) interactions", null, 0);
+        // TODO Auto-generated constructor stub
+    }
+
+    /*
+     * framework
+     */
+
+
+    @Override
+    public void run(MessageReceivedEvent event, String[] args) {
+
+
+        if (player.getStartStatus()) {
+
+
+
+
+
+
+        } else {
+            send(event.getGuild(), event.getChannel(), "You haven't started yet!", true);
+            return;
+        }
+
+    }
+}
