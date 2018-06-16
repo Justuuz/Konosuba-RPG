72110c78abca19e1aaad29fb7dba040b40172879
Thrump <vonchude@gmail.com>
2018-06-16 13:49:49 -0400

    Changes

---
+++ src/main/java/io/osu/konosuba/commands/Help.java
@@ -0,0 +1,23 @@
+package io.osu.konosuba.commands;
+
+import java.awt.Color;
+
+import io.osu.konosuba.Command;
+import io.osu.konosuba.Konosuba;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class Help extends Command{
+
+	protected Help() {
+		super(Konosuba.COLOR, Konosuba.PREFIX, "help", "List all commands", 0, rnull);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void run(MessageReceivedEvent event, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
